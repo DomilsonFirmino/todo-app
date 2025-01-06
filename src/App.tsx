a3e3542a0e24ac03e1b278740286c747769c9cb9
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-01-06 13:00:16 +0100

    feat: Delete Todo, Update Tudo

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,49 @@ function App() {
   const [description, setDescription] = useState("")
 
   const id = Todos.length + 1; 
-  function handleSubmit(e: FormEvent<HTMLFormElement>){
+  function handleSubmitTodo(e: FormEvent<HTMLFormElement>){
     e.preventDefault()
     if(description.length < 1)
       return
     const newTodo:TodoType = {id, description, status: "active"}
-    setTodos([...Todos, newTodo])
+    setTodos([newTodo, ...Todos])
     setDescription("")
   }
+
+  function handleDeleteTodo(id: number){
+    setTodos(Todos.filter((Todo)=>Todo.id != id))
+  }
+
+  function handleCheckTodo(id: number){
+    setTodos(
+      Todos.map( Todo => {
+        if(Todo.id != id){
+          return Todo
+        }
+        else{
+          return {...Todo, status: Todo.status == "active" ? "completed": "active"}
+        }
+      })
+    )
+  }
+
   return (
     <div style={{display: 'grid', placeContent: 'center'}}>
       <div>
-        <form onSubmit={(e)=>handleSubmit(e)}>
+        <form onSubmit={(e)=>handleSubmitTodo(e)}>
           <input onChange={(e)=>setDescription(e.target.value)} value={description} type="text"/>
           <button type="submit">enviar</button>
         </form>
       </div>
-      {Todos.map((Todo) => <p key={Todo.id}>{Todo.description}</p>)}
+      {Todos.map((Todo) => (
+        <div key={Todo.id}>
+          <p>{Todo.description}</p>
+          <button onClick={()=>handleCheckTodo(Todo.id)}>
+            {Todo.status=="active" ? "Ativo": "completo"}
+          </button>
+          <button onClick={()=>handleDeleteTodo(Todo.id)}>Delete Todo</button>
+        </div>
+      ))}
     </div>
   )
 }
