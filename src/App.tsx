c374386ca75186bd8076c16b25ded8dc72eb1068
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-01-06 12:42:27 +0100

    feat: Add Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from 'react'
+import { TodoType } from './types'
+import { data } from './staticdata'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todos, setTodos] = useState<TodoType[]>(data)
+  const [description, setDescription] = useState("")
 
+  const id = Todos.length + 1; 
+  function handleSubmit(e: FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    if(description.length < 1)
+      return
+    const newTodo:TodoType = {id, description, status: "active"}
+    setTodos([...Todos, newTodo])
+    setDescription("")
+  }
   return (
-    <>
+    <div style={{display: 'grid', placeContent: 'center'}}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <input onChange={(e)=>setDescription(e.target.value)} value={description} type="text"/>
+          <button type="submit">enviar</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {Todos.map((Todo) => <p key={Todo.id}>{Todo.description}</p>)}
+    </div>
   )
 }
 
---
+++ src/staticdata.ts
@@ -0,0 +1,14 @@
+import { TodoType } from "./types";
+
+export const data:TodoType[] = [
+    {
+        id: 1, 
+        description: "Correr", 
+        status: "active"
+    },
+    {
+        id: 2, 
+        description: "Correr", 
+        status: "active"
+    }
+]
