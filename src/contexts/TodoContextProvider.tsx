80f6413006d6a2682b49c229734ad80df88e565f
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2025-01-09 02:11:35 +0100

    fix: static todos arrays removed

--- src/contexts/TodoContextProvider.tsx
+++ src/contexts/TodoContextProvider.tsx
@@ -1,14 +1,13 @@
 import { ReactNode, useEffect, useState } from "react"
-import { data } from "../staticdata"
 import { TodoType } from "../@types/types"
 import { TodoContext } from './TodoContext'
 
 export default function TodoContextProvider({children}:{children: ReactNode}) {
-    const [Todos, setTodos] = useState<TodoType[]>( ()=>{
+    const [Todos, setTodos] = useState<TodoType[]>(()=>{
         const stored = localStorage.getItem("Todos");
         if(stored)
             return JSON.parse(stored)
-        return data;
+        return []
     })
 
     useEffect(()=>{
